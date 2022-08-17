8eb3aa43ebb4f509e37899725c7a0ef6c51ce8f2
slav b <sbiegluk@ratheractive.tech>
2022-08-17 22:11:09 +0200

    improved tests

--- tests/dir-tpl.spec.js
+++ tests/dir-tpl.spec.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const fs = require("fs")
+const { compareSync, Options, Result } = require("dir-compare");
 const { tplDir } = require('..')
 
 describe("When source directory is templatized", () => {
-    let srcDirPath = path.join(process.cwd(), 'tests', 'test-tpl-dir')
+    let srcDirPath = path.join(process.cwd(), 'tests', 'test-input-dir')
+    let expectedDirPath = path.join(process.cwd(), 'tests', 'test-output-dir')
     let destDirPath = path.join('/tmp', 'test-tpl-out-dir')
     let rules = [
         {
@@ -19,6 +21,27 @@ describe("When source directory is templatized", () => {
         await tplDir(srcDirPath, destDirPath, rules)
     })
 
-    it("", async () => {
+    it("The output is correct", async () => {
+        const diff = compareSync(expectedDirPath, destDirPath, {
+            compareContent: true, compareDate: false,
+            resultBuilder: function (entry1, entry2, state, level, relativePath, options, statistics, diffSet, reason) {
+                let modeDiff = fs.lstatSync(entry1.absolutePath).mode !== fs.lstatSync(entry2.absolutePath).mode
+
+                diffSet.push(
+                    {
+                        name1: entry1.name,
+                        name2: entry2.name,
+                        path1: entry1.path,
+                        path2: entry2.path,
+                        state: modeDiff ? "distinct" : "equal",
+                        reason: modeDiff ? "different-mode" : reason
+                    }
+                )
+            }
+        })
+
+        const diffs = diff.diffSet?.filter(ds => ds.state !== "equal")
+
+        expect(diffs).toStrictEqual([])
     })
-})  
+})
---
+++ tests/test-output-dir/ActualValueOneService/ActualValueOneController.js
@@ -0,0 +1,5 @@
+class ActualValueOneController{
+    getActualValueOne(){
+        return new ActualValueOneEntity()
+    }
+}
